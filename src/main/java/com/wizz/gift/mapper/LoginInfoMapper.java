d659c05837772beddd92be7618ac8b77965552fe
liqiqiorz <1099462011@qq.com>
2020-11-23 11:38:39 +0800

    feat:LoginInfo and MessageRecordMapper finish

---
+++ src/main/java/com/wizz/gift/mapper/LoginInfoMapper.java
@@ -0,0 +1,11 @@
+package com.wizz.gift.mapper;
+
+import com.wizz.gift.entity.model.LoginInfoDo;
+
+/**
+ * @author liqiqi_tql
+ * @date 2020/11/23 -11:34
+ */
+public interface LoginInfoMapper {
+    int addLoginInfo(LoginInfoDo loginInfoDo);
+}
---
+++ src/main/java/com/wizz/gift/mapper/MessageRecordMapper.java
@@ -0,0 +1,11 @@
+package com.wizz.gift.mapper;
+
+import com.wizz.gift.entity.model.MessageRecordDo;
+
+/**
+ * @author liqiqi_tql
+ * @date 2020/11/23 -11:36
+ */
+public interface MessageRecordMapper {
+    int addMessageRecord(MessageRecordDo messageRecordDo);
+}
